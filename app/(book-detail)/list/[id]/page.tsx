54af7ccd15e6fc20c3bb6fcd0215b1ab58eca229
ssundventure <ssundventure@gmail.com>
2024-11-17 23:42:36 +0900

    props 오류수정

--- app/(book-detail)/list/[id]/page.tsx
+++ app/(book-detail)/list/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { BOOK_API_URL } from "../../../constants";
-import styles from "../../../../styles/bookList.module.css"
+import styles from "../../../../styles/bookList.module.css";
 
-export async function generateMetadata(props: { params: { id: any } }){
-    const params = await props.params;
-    const id = params.id;
-    const books = await getBooks(id);
-    return {
-        title : books.results.list_name,
-    };
+type IParams = Promise<{ id: string }>;
+
+export async function generateMetadata(props: { params: IParams }) {
+  const params = await props.params;
+  const id = params.id;
+  const books = await getBooks(id);
+  return {
+    title: books.results.list_name,
+  };
 }
 
 async function getBooks(id: string) {
@@ -16,14 +18,14 @@ async function getBooks(id: string) {
   return json;
 }
 
-export default async function Detail(props: { params: { id: any } }) {
+export default async function Detail(props: { params: IParams }) {
   const params = await props.params;
   const id = params.id;
   const books = await getBooks(id);
 
   // 데이터 확인
   if (!books?.results?.books) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   return (
@@ -32,15 +34,15 @@ export default async function Detail(props: { params: { id: any } }) {
       <div className={styles.grid}>
         {books.results.books.map((book) => (
           <div key={book.primary_isbn13} className={styles.card}>
-            <img 
-              src={book.book_image} 
+            <img
+              src={book.book_image}
               alt={book.title}
               className={styles.image}
             />
             <h3 className={styles.bookTitle}>{book.title}</h3>
             <p className={styles.author}>{book.author}</p>
-            <a 
-              href={book.amazon_product_url} 
+            <a
+              href={book.amazon_product_url}
               target="_blank"
               rel="noopener noreferrer"
               className={styles.buyButton}
